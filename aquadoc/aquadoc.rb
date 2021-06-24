6b1f0c576292d26ef9070a25c4c873c6d09902bf
Aquadoc <info@aquarium.com>
2021-06-24 16:26:57 -0700

    Set up files and change imports to work locally

---
+++ aquadoc/aquadoc.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require_relative 'version.rb'
+require_relative 'make.rb'
+require_relative 'render.rb'
+require_relative 'git.rb'
+require_relative 'local.rb'
---
+++ aquadoc/local.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'erb'
+require 'fileutils'
+
+module Aquadoc
+  class Local
+    def initialize(config, categories, opts = {})
+      puts "In Local initialize"
+      @opts = opts
+      @config = config
+      @repo = nil
+      @categories = categories
+    end
+
+    def run
+      puts "Calling Render!"
+      aquadoc = Render.new(self, @config, @categories)
+      FileUtils.mkdir_p 'docs'
+      FileUtils.mkdir_p 'docs/operation_types'
+      FileUtils.mkdir_p 'docs/libraries'
+      FileUtils.mkdir_p 'docs/sample_types'
+      FileUtils.mkdir_p 'docs/object_types'
+      FileUtils.mkdir_p 'docs/css'
+#      FileUtils.mkdir_p 'docs/js'
+      aquadoc.make(@opts)
+    end
+
+    def write(path, content)
+      puts '--> ' + 'docs/' + path
+      File.write('docs/' + path, content)
+    end
+
+    def authorized
+      true
+    end
+  end
+end
